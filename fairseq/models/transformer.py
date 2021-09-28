3fc906831e5b7f6b0343b98529b2e39813610136
Shuo Wang <315665262@qq.com>
2021-09-28 22:19:30 +0800

    debug

--- fairseq/models/transformer.py
+++ fairseq/models/transformer.py
@@ -946,7 +946,6 @@ class TransformerDecoder(FairseqIncrementalDecoder):
                 may_end_idx = (self.padding_idx * may_end_mask + self.dictionary.eos() * (~may_end_mask)).unsqueeze(-1).long() # B x 1 x 1
                 may_end_src = torch.zeros_like(may_end_idx).float() + 1e-8
                 model_prob = model_prob.scatter(dim=-1, index=may_end_idx, src=may_end_src)
-                model_prob[:,:,self.dictionary.eos()] = 1e-10
                 selected_tok = model_prob.argmax(dim=-1) # B x 1
                 selected_mask = tgt_v_toks.eq(selected_tok)
                 new_tgt_v_toks = tgt_v_toks * (~selected_mask) + self.padding_idx * selected_mask # B x T(v)
