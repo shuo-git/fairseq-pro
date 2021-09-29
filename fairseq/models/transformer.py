8402e2bb9d323a8f86c498155c5c1f2379da0ebd
Shuo Wang <315665262@qq.com>
2021-09-29 20:19:30 +0800

    debug

--- fairseq/models/transformer.py
+++ fairseq/models/transformer.py
@@ -821,6 +821,11 @@ class TransformerDecoder(FairseqIncrementalDecoder):
                 tgt_v_toks = saved_state['target_value']
             else:
                 saved_state = {}
+            selected_tok = prev_output_tokens # B x 1
+            selected_mask = tgt_v_toks.eq(selected_tok)
+            tgt_v_toks = tgt_v_toks * (~selected_mask) + self.padding_idx * selected_mask # B x T(v)
+            saved_state['target_value'] = tgt_v_toks
+            self.set_incremental_state(incremental_state, 'plug_in_state', saved_state)
         if tgt_k_toks is not None and tgt_v_toks is not None:
             if self.args.encoder_out_key and kwargs.get('src_wil', None) is not None:
                 encoder_out_h = encoder_out.encoder_out.transpose(0, 1)
@@ -946,11 +951,6 @@ class TransformerDecoder(FairseqIncrementalDecoder):
                 may_end_idx = (self.padding_idx * may_end_mask + self.dictionary.eos() * (~may_end_mask)).unsqueeze(-1).long() # B x 1 x 1
                 may_end_src = torch.zeros_like(may_end_idx).float() + 1e-8
                 model_prob = model_prob.scatter(dim=-1, index=may_end_idx, src=may_end_src)
-                # selected_tok = model_prob.argmax(dim=-1) # B x 1
-                # selected_mask = tgt_v_toks.eq(selected_tok)
-                # new_tgt_v_toks = tgt_v_toks * (~selected_mask) + self.padding_idx * selected_mask # B x T(v)
-                # saved_state['target_value'] = new_tgt_v_toks
-                # self.set_incremental_state(incremental_state, 'plug_in_state', saved_state)
 
         return logits, {"attn": [attn], "inner_states": inner_states, "model_prob": model_prob}
 
