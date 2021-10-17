55153b872b871251a9d04abca05eb445aee3f96c
Shuo Wang <315665262@qq.com>
2021-10-17 12:03:04 +0800

    debug

--- fairseq_cli/train.py
+++ fairseq_cli/train.py
@@ -71,6 +71,10 @@ def main(args):
             p.requires_grad = False
         for p in model.encoder.embed_prefix.parameters():
             p.requires_grad = True
+        for p in model.encoder.plug_ins.parameters():
+            p.requires_grad = True
+        for p in model.encoder.kv_aggregator.parameters():
+            p.requires_grad = True
         for p in model.decoder.plug_ins.parameters():
             p.requires_grad = True
     criterion = task.build_criterion(args)
