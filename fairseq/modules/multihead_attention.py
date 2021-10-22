6c2917393994c838fba1f56e03facca451ec5cf0
Shuo Wang <315665262@qq.com>
2021-10-22 12:49:12 +0800

    debug

--- fairseq/modules/multihead_attention.py
+++ fairseq/modules/multihead_attention.py
@@ -100,11 +100,13 @@ class MultiheadAttention(nn.Module):
             # Empirically observed the convergence to be much better with
             # the scaled initialization
             nn.init.xavier_uniform_(self.k_proj.weight, gain=1 / math.sqrt(2))
-            nn.init.xavier_uniform_(self.v_proj.weight, gain=1 / math.sqrt(2))
+            if self.v_proj is not None:
+                nn.init.xavier_uniform_(self.v_proj.weight, gain=1 / math.sqrt(2))
             nn.init.xavier_uniform_(self.q_proj.weight, gain=1 / math.sqrt(2))
         else:
             nn.init.xavier_uniform_(self.k_proj.weight)
-            nn.init.xavier_uniform_(self.v_proj.weight)
+            if self.v_proj is not None:
+                nn.init.xavier_uniform_(self.v_proj.weight)
             nn.init.xavier_uniform_(self.q_proj.weight)
 
         nn.init.xavier_uniform_(self.out_proj.weight)
